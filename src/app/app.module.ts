7b356cd22c65de6eb51cf519749249b4297ac93b
Slimcent <33299753+Slimcent@users.noreply.github.com>
2022-08-21 17:51:30 +0100

    task and task-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+//import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from "./Task"
+
+export const TASKS : Task[] = [
+    {
+        id : 1,
+        text : "Doctor's appointment",
+        day : "May 5th at 2:30pm",
+        reminder : true
+    },
+    {
+        id : 2,
+        text : "School run",
+        day : "May 6th at 2:30pm",
+        reminder : true
+    }
+]
